3ae166f97d45afa9bfe51f3f86d9786f5f8babc6
Robert Jackson <robert.w.jackson@me.com>
2016-04-21 18:51:17 -0400

    Use dot separated for dirs with a single file.

--- test/fixtures/classic-acceptance/output.js
+++ test/fixtures/classic-acceptance/output.js
@@ -2,45 +2,67 @@ module.exports = {
   'src': {
     'main.js': 'app.js',
     'components': {
-      'foo-bar.component.js': 'foo-bar component',
-      'foo-bar.template.hbs': 'foo-bar component template',
-      'baz-derp.component.js': 'baz-derp component',
-      'baz-derp.template.hbs': 'baz-derp template'
+      'foo-bar': {
+        'component.js': 'foo-bar component',
+        'template.hbs': 'foo-bar component template'
+      },
+      'baz-derp': {
+        'component.js': 'baz-derp component',
+        'template.hbs': 'baz-derp template'
+      }
     },
     'helpers': {
       'i18n.js': 'i18n helper',
       'blerg.js': 'blerg helper'
     },
     'routes': {
-      'index.controller.js': 'index controller',
-      'index.route.js': 'index route',
-      'index.template.hbs': 'index route template',
+      'index': {
+        'controller.js': 'index controller',
+        'route.js': 'index route',
+        'template.hbs': 'index route template'
+      },
       'posts': {
-        'index.controller.js': 'posts/index controller',
-        'index.route.js': 'posts/index route',
-        'index.template.hbs': 'posts/index route template',
-        'post': {
-          'index.controller.js': 'posts/post/index controller',
-          'index.route.js': 'posts/post/index route',
-          'index.template.hbs': 'posts/post/index route template',
-          'edit.controller.js': 'posts/post/edit controller',
-          'edit.route.js': 'posts/post/edit route',
-          'edit.template.hbs': 'posts/post/edit route template'
+        'index': {
+          'controller.js': 'posts/index controller',
+          'route.js': 'posts/index route',
+          'template.hbs': 'posts/index route template'
         },
-        'new.controller.js': 'posts/new controller',
-        'new.route.js': 'posts/new route',
-        'new.template.hbs': 'posts/new route template'
+        'post': {
+          'index': {
+            'controller.js': 'posts/post/index controller',
+            'route.js': 'posts/post/index route',
+            'template.hbs': 'posts/post/index route template'
+          },
+          'edit': {
+            'controller.js': 'posts/post/edit controller',
+            'route.js': 'posts/post/edit route',
+            'template.hbs': 'posts/post/edit route template'
+          }
+        },
+        'new': {
+          'controller.js': 'posts/new controller',
+          'route.js': 'posts/new route',
+          'template.hbs': 'posts/new route template'
+        },
+        'show.template.hbs': 'posts/post/show route template'
       }
     },
     'models': {
-      'application.adapter.js': 'application adapter',
-      'application.serializer.js': 'application serializer',
-      'post.adapter.js': 'post adapter',
-      'post.serializer.js': 'post serializer',
-      'post.model.js': 'post model',
-      'comment.adapter.js': 'comment adapter',
-      'comment.serializer.js': 'comment serializer',
-      'comment.model.js': 'comment model'
+      'application': {
+        'adapter.js': 'application adapter',
+        'serializer.js': 'application serializer'
+      },
+      'post': {
+        'adapter.js': 'post adapter',
+        'serializer.js': 'post serializer',
+        'model.js': 'post model'
+      },
+      'comment': {
+        'adapter.js': 'comment adapter',
+        'serializer.js': 'comment serializer',
+        'model.js': 'comment model'
+      },
+      'tag.model.js': 'tag model'
     },
     'initializers': {
       'blah.js': 'blah initializer',
