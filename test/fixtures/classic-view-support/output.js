e8c570b4e679611b972b4a9747c50e734f092a89
Robert Jackson <robert.w.jackson@me.com>
2016-04-24 23:05:26 -0400

    Add support for views.
    Views that are not invoked via templates are put in `src/ui/routes`
    and those that are template invoked (via string literal view name) are
    put into `src/ui/views`.
    
    Also adds `repopulate` hook to allow file infos to do work to detect usage
    and tweak resulting collection/name/etc.

---
+++ test/fixtures/classic-view-support/output.js
@@ -0,0 +1,18 @@
+module.exports = {
+  'src': {
+    'ui': {
+      'views': {
+        'bar.js': 'bar view with template invocation'
+      },
+      'routes': {
+        'index': {
+          'template.hbs': '{{view "bar"}}'
+        },
+        'foo': {
+          'view.js': 'foo view no template invocation',
+          'template.hbs': 'foo template'
+        }
+      }
+    }
+  }
+};
