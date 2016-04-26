cb31e76d443f00ca9abb5046b0d58ed91621f1bf
Robert Jackson <robert.w.jackson@me.com>
2016-04-26 17:25:27 -0400

    Remove directories that only contain .gitkeep files.

---
+++ test/fixtures/directory-cleanup/input.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'app': {
+    'routes': {
+      '.gitkeep': '',
+      'post.js': 'post route'
+    },
+    'templates': {
+      '.gitkeep': '',
+      'post.hbs': 'post route template'
+    }
+  },
+  'tests': {
+    'unit': {
+      '.gitkeep': ''
+    },
+    'integration': {
+      '.gitkeep': ''
+    }
+  }
+};
