b82f0db1efb71de0eaec619e7562526510f48f44
Andreas Andreou <andreoua@gmail.com>
2017-07-08 02:31:48 +0300

    Fix config import rewriting to only affect the root config

--- test/fixtures/config/input.js
+++ test/fixtures/config/input.js
@@ -2,8 +2,11 @@ module.exports = {
   app: {
     'app.js': 'import config from "./config/environment";',
     utils: {
-      'first.js': 'import config from "my-app/config/environment";',
-      'second.js': 'import config from "../config/environment";'
+      'first.js': 'import ConfigUtils from "./config/third";',
+      'second.js': 'import config from "../config/environment";',
+      config: {
+        'third.js': 'import config from "../../config/environment";',
+      }
     }
   },
   config: {
@@ -13,7 +16,10 @@ module.exports = {
     unit: {
       utils: {
         'first-test.js': 'import config from "../../../config/environment";',
-        'second-test.js': 'import config from "my-app/config/environment";'
+        'second-test.js': 'import config from "my-app/config/environment";',
+        config: {
+          'third-test.js': 'import config from "my-app/config/environment";'
+        }
       }
     }
   }
